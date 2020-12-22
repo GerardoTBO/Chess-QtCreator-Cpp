d949e247166a84c051b8facc62358e455acd1e95
DarkLobox <lgomezp@unsa.edu.pe>
2020-12-22 18:43:27 -0500

    Iconos y archivos faltantes

--- builderboard.cpp
+++ builderboard.cpp
@@ -15,24 +15,24 @@ void BuilderBoard::assingPieces(Piece* boxes[8][8],QString type){
 }
 void BuilderBoard::testBoard(Piece* boxes[8][8]){
     //Piezas Blancas
-//    builderPieces->createPiece(0,0,true,"R","white",boxes);
-//    builderPieces->createPiece(0,1,false,"N","white",boxes);
-//    builderPieces->createPiece(0,2,true,"B","white",boxes);
-//    builderPieces->createPiece(0,3,false,"Q","white",boxes);
+    builderPieces->createPiece(0,0,true,"R","white",boxes);
+    builderPieces->createPiece(0,1,false,"N","white",boxes);
+    builderPieces->createPiece(0,2,true,"B","white",boxes);
+    builderPieces->createPiece(0,3,false,"Q","white",boxes);
     builderPieces->createPiece(0,4,true,"K","white",boxes);
-//    builderPieces->createPiece(0,5,false,"B","white",boxes);
-//    builderPieces->createPiece(0,6,true,"N","white",boxes);
-//    builderPieces->createPiece(0,7,false,"R","white",boxes);
+    builderPieces->createPiece(0,5,false,"B","white",boxes);
+    builderPieces->createPiece(0,6,true,"N","white",boxes);
+    builderPieces->createPiece(0,7,false,"R","white",boxes);
 
     //Piezas Negras
-//    builderPieces->createPiece(7,0,false,"R","black",boxes);
-//    builderPieces->createPiece(7,1,true,"N","black",boxes);
-//    builderPieces->createPiece(7,2,false,"B","black",boxes);
-//    builderPieces->createPiece(7,3,true,"Q","black",boxes);
+    builderPieces->createPiece(7,0,false,"R","black",boxes);
+    builderPieces->createPiece(7,1,true,"N","black",boxes);
+    builderPieces->createPiece(7,2,false,"B","black",boxes);
+    builderPieces->createPiece(7,3,true,"Q","black",boxes);
     builderPieces->createPiece(7,4,false,"K","black",boxes);
-//    builderPieces->createPiece(7,5,true,"B","black",boxes);
-//    builderPieces->createPiece(7,6,false,"N","black",boxes);
-//    builderPieces->createPiece(7,7,true,"R","black",boxes);
+    builderPieces->createPiece(7,5,true,"B","black",boxes);
+    builderPieces->createPiece(7,6,false,"N","black",boxes);
+    builderPieces->createPiece(7,7,true,"R","black",boxes);
 }
 
 void BuilderBoard::resetBoard(Piece* boxes[8][8]){
